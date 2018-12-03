97fcbe5ffaeea3deb57ff472f81cbb6df2ea6dd3
Andrii Holub <andr.holub@gmail.com>
2018-12-03 21:27:26 +0200

    mock dbdata

--- src/main/java/guru/springframework/spring5webapp/repositories/Author.java
+++
@@ -1,6 +0,0 @@
-package guru.springframework.spring5webapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface Author extends CrudRepository<Author, Long> {
-}
